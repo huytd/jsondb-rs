27ad8a80ec98b47e5bea568997d3df83a206e063
Huy Tran <kingbazoka@gmail.com>
2020-04-22 10:51:50 -0700

    Migrate to actix-web 2.0

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use diesel::pg::PgConnection;
 use diesel::r2d2::ConnectionManager;
 use dotenv;
 use actix_web::{HttpServer, App, web, HttpResponse, middleware};
+use actix_files as fs;
 use crate::model::{Store, NewStore};
 
 mod model;
@@ -19,25 +20,7 @@ use serde_json::Value;
 
 type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
-const DESCRIPTION: &str = r#"
-JSONdb is a RESTful JSON storage service that you can
-use to quickly develop your mobile and web app, without
-the need of any backend.
-
-POST  /storage       create a new JSON entry
-GET   /storage/{id}  read a JSON entry
-PUT   /storage/{id}  update a JSON entry
-
-You can also host JSONdb on your own server, or a free
-Heroku dyno, visit: https://github.com/huytd/jsondb-rs/
-for more detail.
-"#;
-
-fn index() -> HttpResponse {
-    HttpResponse::from(DESCRIPTION)
-}
-
-fn create_store(request_data: web::Json<serde_json::Value>, pool: web::Data<Pool>) -> HttpResponse {
+async fn create_store(request_data: web::Json<serde_json::Value>, pool: web::Data<Pool>) -> HttpResponse {
     let serialized = request_data.to_string();
     let uuid = format!("{}", uuid::Uuid::new_v4());
     let new_entry = NewStore {
@@ -53,7 +36,7 @@ fn create_store(request_data: web::Json<serde_json::Value>, pool: web::Data<Pool
     HttpResponse::InternalServerError().into()
 }
 
-fn read_store(api_key: web::Path<String>, pool: web::Data<Pool>) -> HttpResponse {
+async fn read_store(api_key: web::Path<String>, pool: web::Data<Pool>) -> HttpResponse {
     let conn = &pool.get().unwrap();
     if let Ok(mut result) = stores.filter(api_id.eq(api_key.into_inner())).load::<model::Store>(conn) {
         let entry: Store = result.pop().unwrap();
@@ -62,7 +45,7 @@ fn read_store(api_key: web::Path<String>, pool: web::Data<Pool>) -> HttpResponse
     HttpResponse::InternalServerError().into()
 }
 
-fn update_store(api_key: web::Path<String>, request_data: web::Json<serde_json::Value>, pool: web::Data<Pool>) -> HttpResponse {
+async fn update_store(api_key: web::Path<String>, request_data: web::Json<serde_json::Value>, pool: web::Data<Pool>) -> HttpResponse {
     let conn = &pool.get().unwrap();
     let candidates = stores.filter(api_id.eq(api_key.into_inner()));
     if let Ok(result) = diesel::update(candidates).set(data.eq(request_data.to_string())).get_result::<model::Store>(conn) {
@@ -71,7 +54,8 @@ fn update_store(api_key: web::Path<String>, request_data: web::Json<serde_json::
     HttpResponse::InternalServerError().into()
 }
 
-fn main() -> std::io::Result<()> {
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
     dotenv::dotenv().ok();
@@ -86,13 +70,14 @@ fn main() -> std::io::Result<()> {
         App::new()
             .data(pool.clone())
             .wrap(middleware::Logger::default())
-            .service(web::resource("/").route(web::get().to(index)))
             .service(web::resource("/storage").route(web::post().to(create_store)))
             .service(web::resource("/storage/{id}")
-                .route(web::get().to_async(read_store))
-                .route(web::put().to_async(update_store))
+                .route(web::get().to(read_store))
+                .route(web::put().to(update_store))
             )
+            .default_service(fs::Files::new("", "./public"))
     })
     .bind(("0.0.0.0", port))?
     .run()
+    .await
 }
